86f6f01bb83ca72f63a533b7c130bb748ec6933e
halberfan <robinmb@outlook.de>
2021-11-13 19:32:42 +0100

    new food values

--- src/main/java/de/ftscraft/survivalminus/utils/Food.java
+++ src/main/java/de/ftscraft/survivalminus/utils/Food.java
@@ -7,67 +7,57 @@ public enum Food {
 
     //Name Kohlenhydrate, Proteine, Vitamine, Durst, Material - Material ist null bei speziellen Items
 
-    /*OFENKARTOFFEL(1, 0, 0, 0, Material.BAKED_POTATO),
-    KARTOFFEL(1, 0, 0, 0, Material.POTATO),
-    SEETANG(0, -1, 0, 2, Material.DRIED_KELP),
-    ROTE_BEETE(0, 0, 2, 0, Material.BEETROOT),
-    KEKSE(0, 0, 0, 0, Material.COOKIE),
-    GEBRATENER_KABELJAU(0, 1, 0, -1, Material.COOKED_SALMON),
-    GEBRATENER_LACHS(0, 1, 0, -1, Material.COOKED_COD),
-    MELONE(-1, 0, 1, 1, Material.MELON_SLICE),
-    KAROTTE(0, 0, 1, 0, Material.CARROT),
-    APFEL(0, 0, 4, 0, Material.APPLE),
-    G_APFEL(8, 8, 8, 8, Material.GOLDEN_APPLE),
-    OP_APFEL(20, 20, 20, 20, Material.ENCHANTED_GOLDEN_APPLE),
-    BROT(2, 0, 0, 0, Material.BREAD),
-    GEBRATENER_HASE(0, 2, 0, 0, Material.COOKED_RABBIT),
-    ROTE_BEETE_SUPPE(3, 3, 3, 3, Material.BEETROOT_SOUP),
-    HUEHNCHEN(0, 1, 0, 0, Material.COOKED_CHICKEN),
-    PILZSUPPE(2, -2, 2, 2, Material.MUSHROOM_STEW),
-    HAMMELFLEISCH(0, 1, 0, 0, Material.COOKED_MUTTON),
-    G_KAROTTE(3, 3, 3, 3, Material.GOLDEN_CARROT),
-    KUERBIS_KUCHEN(1, 0, 0, 0, Material.PUMPKIN_PIE),
-    SCHEWINEFLEISCH(0, 2, 0, 0, Material.COOKED_PORKCHOP),
-    STEAK(0, 1, 0, 0, Material.COOKED_BEEF),
-    KANNICHEN_RAGOUT(3, 3, 3, 3, Material.RABBIT_STEW),
-    UBERRESTE(0, 0, 0, 0, Material.ROTTEN_FLESH),
-    KUCHEN(4, 4, 4, 4, Material.CAKE);*/
-
-    OFENKARTOFFEL(1, 0, 0, 0, Material.BAKED_POTATO),
-    KARTOFFEL(0, 0, 0, 0, Material.POTATO),
-    SEETANG(0, 0, 0, 1, Material.DRIED_KELP),
-    ROTE_BEETE(0, 0, 2, 0, Material.BEETROOT),
-    KEKSE(1, 0, 0, 0, Material.COOKIE),
-    GEBRATENER_KABELJAU(0, 1, 0, -1, Material.COOKED_SALMON),
-    GEBRATENER_LACHS(0, 1, 0, -1, Material.COOKED_COD),
-    MELONE(-1, 0, 1, 1, Material.MELON_SLICE),
-    KAROTTE(0, 0, 1, 0, Material.CARROT),
-    APFEL(0, 0, 4, 0, Material.APPLE),
-    G_APFEL(8, 8, 8, 8, Material.GOLDEN_APPLE),
-    OP_APFEL(20, 20, 20, 20, Material.ENCHANTED_GOLDEN_APPLE),
-    BROT(2, 0, 0, 0, Material.BREAD),
-    GEBRATENER_HASE(0, 1, 0, 0, Material.COOKED_RABBIT),
-    ROTE_BEETE_SUPPE(0, 0, 15, 0, Material.BEETROOT_SOUP),
-    HUEHNCHEN(0, 1, 0, 0, Material.COOKED_CHICKEN),
-    PILZSUPPE(0, 0, 0, 0, Material.MUSHROOM_STEW),
-    HAMMELFLEISCH(0, 1, 0, 0, Material.COOKED_MUTTON),
-    G_KAROTTE(0, 0, 0, 0, Material.GOLDEN_CARROT),
-    KUERBIS_KUCHEN(1, 0, 1, 0, Material.PUMPKIN_PIE),
-    SCHEWINEFLEISCH(0, 3, 0, 0, Material.COOKED_PORKCHOP),
-    STEAK(0, 1, 0, 0, Material.COOKED_BEEF),
-    KANNICHEN_RAGOUT(0, 0, 0, 0, Material.RABBIT_STEW),
+    OFENKARTOFFEL(8, 0, 0, 0, Material.BAKED_POTATO),
+    KARTOFFEL(4, 0, 0, 0, Material.POTATO),
+    SEETANG(0, 5, -2, 6, Material.DRIED_KELP),
+    ROTE_BEETE(0, 0, 5, 0, Material.BEETROOT),
+    KEKSE(6, 0, 0, 0, Material.COOKIE),
+    GEBRATENER_KABELJAU(0, 8, 0, 0, Material.COOKED_SALMON),
+    GEBRATENER_LACHS(0, 8, 0, 0, Material.COOKED_COD),
+    MELONE(0, 0, 5, 6, Material.MELON_SLICE),
+    KAROTTE(0, 0, 6, 0, Material.CARROT),
+    APFEL(0, 0, 10, 0, Material.APPLE),
+    G_APFEL(8, 8, 25, 4, Material.GOLDEN_APPLE),
+    OP_APFEL(20, 20, 20, 20, Material.ENCHANTED_GOLDEN_APPLE), //
+    BROT(12, 0, 0, -2, Material.BREAD),
+    GEBRATENER_HASE(0, 12, 0, 0, Material.COOKED_RABBIT),
+    ROTE_BEETE_SUPPE(0, 0, 25, 5, Material.BEETROOT_SOUP),
+    HUEHNCHEN(0, 12, 0, 0, Material.COOKED_CHICKEN),
+    PILZSUPPE(10, 0, 8, 8, Material.MUSHROOM_STEW),
+    HAMMELFLEISCH(0, 12, 0, 0, Material.COOKED_MUTTON),
+    G_KAROTTE(0, 0, 15, 0, Material.GOLDEN_CARROT),
+    KUERBIS_KUCHEN(12, 0, 0, 0, Material.PUMPKIN_PIE),
+    SCHEWINEFLEISCH(0, 16, 0, 0, Material.COOKED_PORKCHOP),
+    STEAK(0, 10, 0, 0, Material.COOKED_BEEF),
+    KANNICHEN_RAGOUT(8, 18, 8, 8, Material.RABBIT_STEW),
+    CHORUSFRUCH(-4, 0, 5, -8, Material.CHORUS_FRUIT),
+    GIFTIG_KARTOFFEN(4, 0, 0, 0, Material.POISONOUS_POTATO),
+    HONIG(-2, 0, 0, 8, Material.HONEY_BOTTLE),
+    LEUCHTBEERE(0, 0, 5, 0, Material.GLOW_BERRIES),
+    SELTSAME_SUPPE(5, 5, 5, 5, Material.SUSPICIOUS_STEW),
+    SUESSBEEREN(0, 0, 4, 0, Material.SWEET_BERRIES),
+    SPINNENAUGE(-2, 3, -2, -2, Material.SPIDER_EYE),
+    ZOMBIE_FLEISCH(-2, 3, -2, -2, Material.ROTTEN_FLESH),
+    KUGELFISCH(0, 2, 0, 4, Material.PUFFERFISH),
+    CLOWNFISCH(0, 2, 0, 0, Material.TROPICAL_FISH),
+    R_KABELJAU(0, 4, 0, 0, Material.COD),
+    R_LACHS(0, 4, 0, 0, Material.SALMON),
+    R_HAMMEL(0, 4, 0, 0, Material.MUTTON),
+    R_HASE(0, 4, 0, 0, Material.RABBIT),
+    R_RIND(0, 4, 0, 0, Material.BEEF),
+    R_SCHWEIN(0, 4, 0, 0, Material.PORKCHOP),
+    //FTS-ESSEN
     UBERRESTE(0, 0, 0, 0, null),
-    SUSSERFISCH(0, 2, 0, 0, null),
-    HONIGMELONENSAFT(0, 0, 2, 2, null),
-    MARMELADE(3, 0, 0, 0, null),
-    KUCHEN(0, 0, 0, 0, Material.CAKE);
+    SUSSERFISCH(0, 12, 0, 0, null),
+    HONIGMELONENSAFT(0, 0, 0, 20, null),
+    MARMELADE(16, 0, 8, 0, null);
 
-    private int
+    private final int
             kohlenhydrate,
             vitamine,
             proteine,
             durst;
-    private Material material;
+    private final Material material;
 
     Food(int kh, int protein, int vitamin, int durst, Material mat) {
         this.kohlenhydrate = kh;
@@ -106,31 +96,31 @@ public enum Food {
             //Wenn das Food ein besonderes Essen ist (mat == null) wird geschaut, ob das Item damit übereinstimmt
             if (a.getMaterial() == null) {
                 if (is.getItemMeta() != null) {
-                    if (is.getItemMeta().getDisplayName() != null) {
-                        switch (a) {
-                            case UBERRESTE:
-                                if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_UBERRESTE_NAME)) {
-                                    return a;
-                                }
-                                break;
-                            case SUSSERFISCH:
-                                if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_SUSSERFISCH_NAME)) {
-                                    return a;
-                                }
-                                break;
-                            case HONIGMELONENSAFT:
-                                if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_HONIGMELONENSAFT_NAME)) {
-                                    return a;
-                                }
-                                break;
-                            case MARMELADE:
-                                if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_MARMELADE_NAME)) {
-                                    return a;
-                                }
-                                break;
+                    switch (a) {
+                        case UBERRESTE:
+                            if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_UBERRESTE_NAME)) {
+                                return a;
+                            }
+                            break;
+                        case SUSSERFISCH:
 
-                        }
+                            if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_SUSSERFISCH_NAME)) {
+                                return a;
+                            }
+                            break;
+                        case HONIGMELONENSAFT:
+                            if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_HONIGMELONENSAFT_NAME)) {
+                                return a;
+                            }
+                            break;
+                        case MARMELADE:
+                            if (is.getItemMeta().getDisplayName().equalsIgnoreCase(Variables.ITEM_MARMELADE_NAME)) {
+                                return a;
+                            }
+                            break;
+
                     }
+
                 }
                 //wenn es nicht übereinstimmt, wird das nächste food überprüft
                 continue;
